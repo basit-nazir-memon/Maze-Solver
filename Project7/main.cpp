1b434f9c38812b39791ef91641fe221e762e9747
Basit Nazir <basitnazir585@gmail.com>
2023-06-13 23:37:35 +0500

    Update main.cpp

--- Project7/main.cpp
+++ Project7/main.cpp
@@ -1,20 +1,6 @@
 #include "Maze.h"
-#include "FeedbackQueue.h"
 int main() {
-    cout << "-------------------- Question : 01 -----------------------\n";
-
-    Driver driver;
-    cout << "The Processess in Waiting Queue Are: \n";
-    driver.getWaitingQueue().print();
-    driver.execute();
-
-    /*cout << "Multi Level Queue Name: " << d.getName() << endl;
-    cout << "\n\n\n\n\nAfter Execution, The Processess in Waiting Queue Are: \n";
-    driver.getWaitingQueue().print();*/
-
-    cout << "\n\n\n\n\n\n\n\n\n\n\n\n-------------------- Question : 02 -----------------------\n";
-
-   
+   	cout << "-------------------- MAZE SOLVER -----------------------\n";
 	MazeSolver maze;
 	maze.solve();
 	return 0;
